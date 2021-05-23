df35a6f4deba66ca2d70a9e7e3e0ec856eab9df1
DanielVera33 <legositka@gmail.com>
2021-05-23 18:44:51 +0200

    Avance anadir listas y mejora pantalla principal

--- src/mainProgram/MainInterfaz.java
+++ src/mainProgram/MainInterfaz.java
@@ -26,6 +26,9 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+
+import signUpAuth.SignUp;
+
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 
@@ -40,8 +43,7 @@ public class MainInterfaz extends JFrame {
 	String passText;
 	String userCodeText;
 	String persistencia;
-	String ayuda = "<html><body>En esta ventana debe introducir sus datos. Ejmp: Usuario: Jose, Password: 1234, <br>codUsuario: kj25 (Sus Iniciales y dos numeros seguidos sin espacio, <br>es importante que lo recuerde.)</html></body>";
-	String error = "<html><body>Ocurrio un error en la app, por favor revise si introdujo bien sus datos, <br>si no se trata de eso reinicie la app, si ocurrio otro tipo de problema instale <br>y desistale o compruebe la version.</html></body>";
+	int contadorListas;
 	int ancho = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
     int alto = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
 	
@@ -61,7 +63,11 @@ public class MainInterfaz extends JFrame {
 		});
 	}
 
-	public MainInterfaz() {
+	public MainInterfaz() throws IOException {
+		initComponents();
+	}
+	
+	private void initComponents() {
 		System.out.println("Usuario en pantalla principal");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds((ancho) - (this.getWidth()), (alto) - (this.getHeight()), 1000, 700);
@@ -76,9 +82,9 @@ public class MainInterfaz extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(189, 100, 805, 571);
-		contentPane.add(scrollPane);
+		JLabel lblMisListas = new JLabel("Mis Listas");
+		lblMisListas.setBounds(186, 82, 144, 14);
+		contentPane.add(lblMisListas);
 		
 		JLabel lblListas = new JLabel("Listas");
 		lblListas.setHorizontalAlignment(SwingConstants.CENTER);
@@ -88,7 +94,7 @@ public class MainInterfaz extends JFrame {
 		contentPane.add(lblListas);
 		
 		JSeparator separator = new JSeparator();
-		separator.setBounds(-19, 87, 1013, 8);
+		separator.setBounds(-19, 65, 1013, 8);
 		contentPane.add(separator);
 		
 		JSeparator separator_1 = new JSeparator();
@@ -103,9 +109,83 @@ public class MainInterfaz extends JFrame {
 		lblListphy.setBounds(10, 24, 144, 43);
 		contentPane.add(lblListphy);
 		
-		JButton btnCompletadas = new JButton("Completadas");
+		JButton btnCompletadas = new JButton("Mis Listas");
+		btnCompletadas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Usuario en seccion Mis Listas");
+				lblMisListas.setText("Mis Listas");
+			}
+		});
 		btnCompletadas.setBounds(10, 124, 144, 23);
 		contentPane.add(btnCompletadas);
+		
+		JButton btnFavoritas = new JButton("Favoritas");
+		btnFavoritas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Usuario en seccion Favoritas");
+				lblMisListas.setText("Favoritas");
+			}
+		});
+		btnFavoritas.setBounds(10, 169, 144, 23);
+		contentPane.add(btnFavoritas);
+		
+		JButton btnCompletadas_1 = new JButton("Completadas");
+		btnCompletadas_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Usuario en seccion Completadas");
+				lblMisListas.setText("Completadas");
+			}
+		});
+		btnCompletadas_1.setBounds(10, 213, 144, 23);
+		contentPane.add(btnCompletadas_1);
+		
+		JButton btnNoCompletadas = new JButton("No Completadas");
+		btnNoCompletadas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Usuario en seccion No Completadas");
+				lblMisListas.setText("No Completadas");
+			}
+		});
+		btnNoCompletadas.setBounds(10, 259, 144, 23);
+		contentPane.add(btnNoCompletadas);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(186, 113, 796, 526);
+		contentPane.add(scrollPane);
+		
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				setVisible(false);
+				dispose();
+				System.exit(0);
+			}
+		});
+		btnSalir.setBounds(36, 637, 89, 23);
+		contentPane.add(btnSalir);
+		
+		JLabel lblTotalListas = new JLabel("Total Listas:");
+		lblTotalListas.setBounds(825, 650, 75, 14);
+		contentPane.add(lblTotalListas);
+		
+		JLabel lblNumeroListas = new JLabel("0");
+		lblNumeroListas.setBounds(925, 650, 46, 14);
+		contentPane.add(lblNumeroListas);
+		
+		JButton btnNuevaLista = new JButton("Nueva Lista +");
+		btnNuevaLista.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				try(){
+				contadorListas++;
+				lblNumeroListas.setText(String.valueOf(contadorListas));
+				setVisible(false);
+				CrearLista crearLista = new CrearLista();
+				crearLista.setVisible(true);
+				}
+			}
+		});
+		btnNuevaLista.setBounds(864, 78, 120, 23);
+		contentPane.add(btnNuevaLista);
 		setLocationRelativeTo(null);
 
 	}
